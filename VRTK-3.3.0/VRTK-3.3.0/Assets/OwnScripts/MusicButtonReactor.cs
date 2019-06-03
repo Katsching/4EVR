9b05c7b2544f4f73fe9a07a4de22d618b8ed4edc
Katsching <kai.chen@gmx.de>
2019-06-03 22:51:23 +0200

    Added a second test project with VRTK and steamvr 1.2.3

---
+++ VRTK-3.3.0/VRTK-3.3.0/Assets/OwnScripts/MusicButtonReactor.cs
@@ -0,0 +1,60 @@
+﻿namespace OwnScripts
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using VRTK.Controllables;
+
+    public class MusicButtonReactor : MonoBehaviour
+    {
+        public VRTK_BaseControllable controllable;
+        public Text displayText;
+        public string outputOnMax = "Maximum Reached";
+        public string outputOnMin = "Minimum Reached";
+
+        public AudioClip sound;
+
+        private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+
+        private void Start()
+        {
+            gameObject.AddComponent<AudioSource>();
+            source.clip = sound;
+        }
+    
+        protected virtual void OnEnable()
+        {
+            controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
+            controllable.ValueChanged += ValueChanged;
+            controllable.MaxLimitReached += MaxLimitReached;
+            controllable.MinLimitReached += MinLimitReached;
+        }
+
+        protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+        {
+            if (displayText != null)
+            {
+                displayText.text = e.value.ToString("F1");
+            }
+        }
+
+        protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMax != "")
+            {
+                Debug.Log(outputOnMax);
+                source.PlayOneShot(sound);
+            }
+        }
+
+        protected virtual void MinLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMin != "")
+            {
+                Debug.Log(outputOnMin);
+            }
+        }
+    }
+
+    
+}
