d2d8d9935278eb429073eaa987f62e03c7b3ba51
Katsching <kai.chen@gmx.de>
2019-06-04 22:58:40 +0200

    * added a keyboard with a input UI field

---
+++ VRTK-3.3.0/VRTK-3.3.0/Assets/OwnScripts/KeyboardScript.cs
@@ -0,0 +1,60 @@
+﻿namespace OwnScripts
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using VRTK.Controllables;
+
+    public class KeyboardScript : MonoBehaviour
+    {
+        public VRTK_BaseControllable controllable;
+        public Text displayText;
+        public string outputOnMax = "Maximum Reached";
+        public string outputOnMin = "Minimum Reached";
+
+        public string letter;
+
+        public GameObject textDescription;
+
+
+        private void Start()
+        {
+
+        }
+    
+        protected virtual void OnEnable()
+        {
+            controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
+            controllable.ValueChanged += ValueChanged;
+            controllable.MaxLimitReached += MaxLimitReached;
+            controllable.MinLimitReached += MinLimitReached;
+        }
+
+        protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+        {
+            if (displayText != null)
+            {
+                displayText.text = e.value.ToString("F1");
+            }
+        }
+
+        protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMax != "")
+            {
+                Debug.Log(outputOnMax);
+                Text t = textDescription.GetComponent<Text>();
+                t.text = t.text + letter;            
+            }
+        }
+
+        protected virtual void MinLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMin != "")
+            {
+                Debug.Log(outputOnMin);
+            }
+        }
+    }
+
+    
+}
