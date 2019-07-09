d07cb143c5bbd21f5248a58a2ace7ac71479bb12
Katsching <kai.chen@gmx.de>
2019-07-10 00:37:45 +0200

    * added a countdown and explosion

---
+++ 4EVR/Assets/OwnAssets/OwnScripts/CountdownScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeLeft;
+    public  ParticleSystem explosion;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+            timeLeft -= Time.deltaTime;
+            string minSec = string.Format("{0}:{1:00}", (int)timeLeft / 60, (int)timeLeft % 60); 
+            Text t = gameObject.GetComponent<Text>();
+            t.text = minSec.ToString();
+
+            if ( timeLeft < 0 )
+            {
+                GameOver();
+            }
+
+
+    }
+
+    private void GameOver(){
+        explosion.Play();
+    }
+}
